daf7889e1c704c5a137a6ca20d02b517f8c25dd7
Dawit-coder <dawit3945@gmail.com>
2025-08-15 01:54:47 +0300

    add react-spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
-import { ScrollTrigger, SplitText } from 'gsap/all';
-import gsap from 'gsap';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Cocktails from './components/Cocktails';
-import About from './components/About';
-import Art from './components/Art';
-import Menu from './components/Menu';
-import Contact from './components/Contact';
+import { useState, useEffect } from "react";
+import { RingLoader } from "react-spinners";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Wait until everything is loaded
+    window.addEventListener("load", () => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen bg-black">
+        <RingLoader size={100} color="#8b5cf6" />
+      </div>
+    );
+  }
+
   return (
     <main>
-        <Navbar/>
-        <Hero/>
-        <Cocktails/>
-        <About/>
-        <Art/>
-        <Menu/>
-        <Contact/>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
